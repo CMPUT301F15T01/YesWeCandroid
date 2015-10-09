315d3613644ad32d313df1cc98c7fef9740e6d11
dylan-ashley <dashley@dashley.name>
2015-10-09 15:40:04 -0600

    integration

--- app/src/androidTest/java/ca/ualberta/trinkettrader/PhotographsOfItemsTests.java
+++ app/src/androidTest/java/ca/ualberta/trinkettrader/PhotographsOfItemsTests.java
@@ -1,11 +1,27 @@
+// Copyright 2015 Andrea McIntosh, Dylan Ashley, Anju Eappen, Jenna Hatchard, Kirsten Svidal, Raghav Vamaraju
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ca.ualberta.trinkettrader;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * Created by anju on 09/10/15.
- */
 public class PhotographsOfItemsTests extends ActivityInstrumentationTestCase2 {
+
+    public PhotographsOfItemsTests(Class activityClass) {
+        super(activityClass);
+    }
+
     public void testAttatchPhotoGraph(){
         Photograph photograph = new Photograph('<path/to/photo>');
         ItemProfile profile = new Item().getProfile();
