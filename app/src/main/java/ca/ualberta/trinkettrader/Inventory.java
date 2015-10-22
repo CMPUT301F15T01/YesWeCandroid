f74915fbda923b1ac337923b7a349cc761fe47ec
dylan-ashley <dashley@dashley.name>
2015-10-21 19:22:16 -0600

    added bare model classes

---
+++ app/src/main/java/ca/ualberta/trinkettrader/Inventory.java
@@ -0,0 +1,9 @@
+package ca.ualberta.trinkettrader;
+
+import java.util.Collection;
+
+/**
+ * Created by dashley on 2015-10-21.
+ */
+public class Inventory implements Collection {
+}
---
+++ app/src/main/java/ca/ualberta/trinkettrader/UserSettings.java
@@ -0,0 +1,7 @@
+package ca.ualberta.trinkettrader;
+
+/**
+ * Created by dashley on 2015-10-21.
+ */
+public class UserSettings {
+}
