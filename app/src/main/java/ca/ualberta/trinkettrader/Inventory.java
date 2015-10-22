dc92f035bcbf4f3878d08b8910a853ec1901cf1b
dylan-ashley <dashley@dashley.name>
2015-10-21 20:05:58 -0600

    made collections be collections

--- app/src/main/java/ca/ualberta/trinkettrader/Inventory.java
+++ app/src/main/java/ca/ualberta/trinkettrader/Inventory.java
@@ -1,9 +1,90 @@
 package ca.ualberta.trinkettrader;
 
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by dashley on 2015-10-21.
  */
 public class Inventory implements Collection {
+
+    private ArrayList<Trinket> trinkets;
+
+    public Inventory() {
+    }
+
+    public Inventory(ArrayList<Trinket> trinkets) {
+        this.trinkets = trinkets;
+    }
+
+    @Override
+    public boolean add(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @NonNull
+    @Override
+    public Trinket[] toArray(Object[] array) {
+        return new Trinket[0];
+    }
 }
