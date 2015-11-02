60b873c5abf63c9a11247f7fa4c26f335e0e3884
dylan-ashley <dashley@dashley.name>
2015-11-01 17:01:15 -0700

    more progress

--- app/src/androidTest/java/ca/ualberta/trinkettrader/PhotographsOfItemsTests.java
+++ app/src/androidTest/java/ca/ualberta/trinkettrader/PhotographsOfItemsTests.java
@@ -571,15 +571,96 @@ public class PhotographsOfItemsTests extends ActivityInstrumentationTestCase2 {
     }
 
     public void testManuallyChoosePhotosToDownloadIfPhotoDownloadDisabled() {
-        /*
-        //This is another test that looks like it should be an activity test.
-        // Changes will be made once those tests are underway.
-        //  Assert that default photo download is disabled
-        User user = new User();
-        UserSettings settings = user.getUserSettings();
-        assertFalse(settings.arePhotosDownloadable);
-        // Select photos to download
-        */
+        // Get the current activity
+        HomePageActivity homePageActivity = (HomePageActivity) getActivity();
+
+
+        /******** DisplayUserProfileActivity ********/
+        {
+            // Set up an ActivityMonitor
+            Instrumentation.ActivityMonitor receiverActivityMonitor =
+                    getInstrumentation().addMonitor(DisplayUserProfileActivity.class.getName(),
+                            null, false);
+
+            // Start DisplayInventoryActivity
+            final Button profileButton = homePageActivity.getProfileButton();
+            homePageActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    profileButton.performClick();
+                }
+            });
+            getInstrumentation().waitForIdleSync();
+
+            // Validate that ReceiverActivity is started
+            DisplayUserProfileActivity receiverActivity = (DisplayUserProfileActivity)
+                    receiverActivityMonitor.waitForActivityWithTimeout(1000);
+            assertNotNull("ReceiverActivity is null", receiverActivity);
+            assertEquals("Monitor for ReceiverActivity has not been called",
+                    1, receiverActivityMonitor.getHits());
+            assertEquals("Activity is of wrong type",
+                    DisplayUserProfileActivity.class, receiverActivity.getClass());
+
+            // Remove the ActivityMonitor
+            getInstrumentation().removeMonitor(receiverActivityMonitor);
+        }
+
+        // Get the new activity
+        DisplayUserProfileActivity displayUserProfileActivity = (DisplayUserProfileActivity) getActivity();
+
+        /******** EditProfileActivity ********/
+        {
+            // Set up an ActivityMonitor
+            Instrumentation.ActivityMonitor receiverActivityMonitor =
+                    getInstrumentation().addMonitor(EditProfileActivity.class.getName(),
+                            null, false);
+
+            // Start DisplayInventoryActivity
+            final Button editUserProfileButton = displayUserProfileActivity.getEditUserProfileButton();
+            homePageActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    editUserProfileButton.performClick();
+                }
+            });
+            getInstrumentation().waitForIdleSync();
+
+            // Validate that ReceiverActivity is started
+            EditProfileActivity receiverActivity = (EditProfileActivity)
+                    receiverActivityMonitor.waitForActivityWithTimeout(1000);
+            assertNotNull("ReceiverActivity is null", receiverActivity);
+            assertEquals("Monitor for ReceiverActivity has not been called",
+                    1, receiverActivityMonitor.getHits());
+            assertEquals("Activity is of wrong type",
+                    EditProfileActivity.class, receiverActivity.getClass());
+
+            // Remove the ActivityMonitor
+            getInstrumentation().removeMonitor(receiverActivityMonitor);
+        }
+
+        // Get the new activity
+        EditProfileActivity editProfileActivity = (EditProfileActivity) getActivity();
+
+
+        UserProfile userProfile = editProfileActivity.getUserProfile();
+        userProfile.setArePhotosDownloadable(Boolean.TRUE);
+        assertTrue(userProfile.getArePhotosDownloadable());
+
+        // Click the button
+        final ToggleButton arePhotosDownloadableButton = editProfileActivity.getArePhotosDownloadableButton();
+        assertTrue(arePhotosDownloadableButton.isChecked());
+        homePageActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                arePhotosDownloadableButton.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        assertFalse(arePhotosDownloadableButton.isChecked());
+        assertFalse(userProfile.getArePhotosDownloadable());
+
+        // Return to main menu
+        editProfileActivity.finish();
+        displayUserProfileActivity.finish();
+
+        /******** Need to try and manually download a photo here ********/
     }
 
     public void testDisablePhotoDownload() {
@@ -651,7 +732,7 @@ public class PhotographsOfItemsTests extends ActivityInstrumentationTestCase2 {
         // Get the new activity
         EditProfileActivity editProfileActivity = (EditProfileActivity) getActivity();
 
-        /******** Disable Photo Downloads ********/
+
         UserProfile userProfile = editProfileActivity.getUserProfile();
         userProfile.setArePhotosDownloadable(Boolean.TRUE);
         assertTrue(userProfile.getArePhotosDownloadable());
@@ -671,5 +752,7 @@ public class PhotographsOfItemsTests extends ActivityInstrumentationTestCase2 {
         // Return to main menu
         editProfileActivity.finish();
         displayUserProfileActivity.finish();
+
+        /******** Need to check here if the photos actually don't download ********/
     }
 }
