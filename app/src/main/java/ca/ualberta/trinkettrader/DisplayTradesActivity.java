59591553867acade6f9c20146bc705a0b317e2c9
theWhyteRabbit <svidal@ualberta.ca>
2015-11-05 20:24:28 -0700

    started work on modelling DisplayTradesActivity after DisplayFriendsActivity

--- app/src/main/java/ca/ualberta/trinkettrader/DisplayTradesActivity.java
+++ app/src/main/java/ca/ualberta/trinkettrader/DisplayTradesActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 /**
  *
@@ -11,11 +13,18 @@ import android.view.View;
  *
  */
 public class DisplayTradesActivity extends AppCompatActivity {
+    Button pastTradesButton;
+    ListView currentTradesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_trades);
+        currentTradesListView = (ListView)findViewById(R.id.friendsView);
+        pastTradesButton = (Button)findViewById(R.id.past_trades_button);
+        // equivalent of user friend's list
+        // controller
+        // set controller
     }
 
     // PastTrades Button click
@@ -23,4 +32,6 @@ public class DisplayTradesActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayPastTradesActivity.class);
         startActivity(intent);
     }
+
+    public Button getPastTradesButton(){return pastTradesButton; }
 }
