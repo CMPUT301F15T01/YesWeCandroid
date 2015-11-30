833acc53ad9277e89baa87069fb85004ef88a75a
Anju Eappen <aeappen@ualberta.ca>
2015-11-30 08:42:30 -0700

    Tracked friends loaded from network, all inventories text searchable

--- app/src/main/java/ca/ualberta/trinkettrader/Friends/AllFriendsInventoriesController.java
+++ app/src/main/java/ca/ualberta/trinkettrader/Friends/AllFriendsInventoriesController.java
@@ -1,5 +1,8 @@
 package ca.ualberta.trinkettrader.Friends;
 
+import ca.ualberta.trinkettrader.Inventory.Inventory;
+import ca.ualberta.trinkettrader.Inventory.Trinket.Trinket;
+
 /**
  * Created by anju on 29/11/15.
  */
@@ -9,5 +12,19 @@ public class AllFriendsInventoriesController {
 
     public AllFriendsInventoriesController(AllFriendsInventoriesActivity activity) {
         this.activity = activity;
+    }
+
+    public void friendsFilterButtonOnClick(){
+        String category = activity.getCategorySpinner().getSelectedItem().toString();
+        String textQuery = activity.getSearchBox().getText().toString();
+        Inventory empty = new Inventory();
+        activity.setInventory(empty);
+
+        for(Trinket t: activity.getCompleteInventory()){
+            if((t.getCategory() == category) && (t.getDescription().contains(textQuery) | t.getName().contains(textQuery))){
+                empty.add(t);
+            }
+        }
+        activity.getTrinketArrayAdapter().notifyDataSetChanged();
     }
 }
